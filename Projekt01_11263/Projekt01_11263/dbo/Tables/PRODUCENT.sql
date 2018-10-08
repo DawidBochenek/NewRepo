9acb12e2823ac0cd089102ce4468165021ab75a6
DawidBochenek <dawid.bochenek@hotmail.com>
2018-10-08 12:13:06 +0200

    Porownanie danych dodane tabele

---
+++ Projekt01_11263/Projekt01_11263/dbo/Tables/PRODUCENT.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PRODUCENT] (
+    [Prd_ShortName]        VARCHAR (10)  NULL,
+    [Prd_FullName]         NVARCHAR (25) NULL,
+    [Prd_NumberOfBranches] INT           NULL,
+    [Prd_CarModels]        INT           NULL,
+    [Prd_MarketValue]      MONEY         NULL,
+    [Prd_ContactNumber]    INT           NULL,
+    [Prd_CreationDate]     DATETIME      DEFAULT (getdate()) NOT NULL,
+    [Prd_ModificationDate] DATETIME      NULL
+);
+
---
+++ Projekt01_11263/Projekt01_11263/dbo/Tables/PRODUKT.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PRODUKT] (
+    [Pro_Name]            NVARCHAR (25) NULL,
+    [Pro_Type]            NVARCHAR (20) NULL,
+    [Pro_NumberOfColours] INT           NULL,
+    [Pro_SupplyState]     INT           NULL,
+    [Pro_Price]           MONEY         NULL
+);
+
