a53a096d671eeaa0916e734ae65159758825c2b9
DawidBochenek <dawid.bochenek@hotmail.com>
2018-10-08 12:23:11 +0200

    PrimeryKey alter table

--- Projekt01_11263/Projekt01_11263/dbo/Tables/PRODUCENT.sql
+++ Projekt01_11263/Projekt01_11263/dbo/Tables/PRODUCENT.sql
@@ -6,6 +6,10 @@
     [Prd_MarketValue]      MONEY         NULL,
     [Prd_ContactNumber]    INT           NULL,
     [Prd_CreationDate]     DATETIME      DEFAULT (getdate()) NOT NULL,
-    [Prd_ModificationDate] DATETIME      NULL
+    [Prd_ModificationDate] DATETIME      NULL,
+    [Prd_IdNumber]         INT           IDENTITY (1, 1) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Prd_IdNumber] ASC)
 );
+
+
 
