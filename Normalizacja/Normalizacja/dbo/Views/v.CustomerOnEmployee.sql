4ccec0e9370cf663e63cd016aad811fa93fa7474
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2018-10-19 19:01:18 +0100

    update 1

---
+++ Normalizacja/Normalizacja/dbo/Views/v.CustomerOnEmployee.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[v.CustomerOnEmployee]
+	AS 
+	SELECT 
+		[cs].[FirstName]
+	,	[cs].[LastName]
+	,	[cs].[Company]
+	,	[em].[FirstName]	as [EmpFirstName]
+	,	[em].[LastName]	as [EmpLastName]
+	,	[em].[Title]
+	FROM Employee as em
+	INNER JOIN Customer as cs on em.EmployeeId= cs.SupportRepId
