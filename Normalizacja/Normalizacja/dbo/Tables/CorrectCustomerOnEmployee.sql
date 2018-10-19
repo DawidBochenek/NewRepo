65ad1d4ef37f6b6fe9827d4028c12e37cf181b2b
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2018-10-19 19:06:12 +0100

    nowa tabela denorm

---
+++ Normalizacja/Normalizacja/dbo/Tables/CorrectCustomerOnEmployee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CorrectCustomerOnEmployee] (
+    [FirstName]    NVARCHAR (40) NOT NULL,
+    [LastName]     NVARCHAR (20) NOT NULL,
+    [Company]      NVARCHAR (80) NULL,
+    [EmpFirstName] NVARCHAR (20) NOT NULL,
+    [EmpLastName]  NVARCHAR (20) NOT NULL,
+    [Title]        NVARCHAR (30) NULL
+);
+
