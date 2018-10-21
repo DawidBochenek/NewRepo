c704c0a9aef0178dbb09fc0c5cb5962f846fb360
dawid.bochenek <dawid.bochenek@PK3-11>
2018-10-21 14:16:13 +0200

    lab 2

---
+++ Database2_lab/Database2_lab/dbo/Views/View5.sql
@@ -0,0 +1,25 @@
+﻿	
+	CREATE VIEW dbo.View5
+	WITH SCHEMABINDING
+	AS
+	SELECT
+		dd.[CalendarMonthLabel] 
+	,	ds.[StoreName]
+	,	AVG(f.[UnitCost]		)	AS [AVG_UnitCost]	
+	,	AVG(f.[UnitPrice]		)	AS [AVG_UnitPrice]	
+	,	SUM(f.[SalesQuantity]	)	AS [SUM_SalesQuantity]
+	FROM 
+				[dbo].[FactSales]		AS f
+	INNER JOIN	[dbo].[DimChannel]		AS dc ON [dc].[ChannelKey]		= [f].[channelKey]
+	INNER JOIN	[dbo].[DimStore]		AS ds ON [ds].[StoreKey]		= [f].[StoreKey]
+	INNER JOIN	[dbo].[DimProduct]		AS dp ON [dp].[ProductKey]		= [f].[ProductKey]
+	INNER JOIN	[dbo].[DimDate]			AS dd ON [dd].[Datekey]			= [f].[DateKey]
+
+	WHERE 1=1
+	AND dd.[CalendarYear]	=	2009
+	AND dc.[ChannelName]	=	'Store'
+	AND ds.[ZipCode]		=	97001
+	AND dp.[BrandName]		=	'Contoso'
+	GROUP BY
+		dd.[CalendarMonthLabel] 
+	,	ds.[StoreName]
---
+++ Database2_lab/Database2_lab/dbo/Views/View6.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[View6] (
+    [CalendarMonthLabel],
+    [StoreName],
+    [AVG_UnitCost],
+    [AVG_UnitPrice],
+    [SUM_SalesQuantity]
+)
+WITH ENCRYPTION
+AS
+SELECT NULL AS [NullColumn]
+--The script body was encrypted and cannot be reproduced here.;
+
