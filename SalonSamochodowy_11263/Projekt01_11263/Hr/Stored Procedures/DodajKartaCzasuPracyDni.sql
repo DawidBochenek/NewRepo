dc1c415a5c2e4bea815a92ef5199da8e100371a5
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-14 07:18:16 +0100

    HR CD

---
+++ SalonSamochodowy_11263/Projekt01_11263/Hr/Stored Procedures/DodajKartaCzasuPracyDni.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE Hr.DodajKartaCzasuPracyDni
+@PracownikId int,
+@MiesiacId int,
+@Dzien int,
+@ObecnoscId int,
+@Od decimal(10,2),
+@Do decimal(10,2)
+AS
+BEGIN
+
+DECLARE @Nadgodziny decimal(10,2)
+
+SET @Nadgodziny = ABS((SELECT @Do-@Od))-8
+
+INSERT INTO Hr.KartaCzasuPracyDni (PracownikId,MiesiacId,Dzien,ObecnoscId,Od,Do,Nadgodziny)
+VALUES (@PracownikId,@MiesiacId,@Dzien,@ObecnoscId,@Od,@Do,@Nadgodziny)
+
+END
---
+++ SalonSamochodowy_11263/Projekt01_11263/Hr/Tables/GodzinowyCzasPracy.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Hr].[GodzinowyCzasPracy] (
+    [RokId]             INT NOT NULL,
+    [MiesiacId]         INT NOT NULL,
+    [LiczbaGodzinPracy] INT NULL,
+    [LiczbaDniPracy]    INT NULL,
+    [LiczbaDniWolnych]  INT NULL,
+    PRIMARY KEY CLUSTERED ([RokId] ASC, [MiesiacId] ASC)
+);
+
